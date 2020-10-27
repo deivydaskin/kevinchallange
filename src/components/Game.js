bc565a5eae0f3b02aba83a972ffaa7a8a4a41135
Deivydas Kinderis <kalted1997@gmail.com>
2020-10-28 00:21:13 +0200

    Added connection to api, fixed some game bugs, completed logs system

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { calculateWinner } from "../helpers";
 import Board from "./Board";
+import { postReq, getReq } from "./ApiRequests";
+import { logReq, getLogs } from "./LogApi";
 
 const styles = {
-  width: "100px",
-  margin: "20px auto",
+  div1: {
+    width: "100px",
+    margin: "20px auto",
+  },
+  div2: {
+    width: "400px",
+    margin: "auto",
+  },
+  button: {
+    width: "100px",
+    margin: "20px auto auto 0px",
+    height: "30px",
+  },
 };
 
 const Game = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(squares);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    getReq().then((data) => {
+      setSquares(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    getLogs().then((data) => {
+      setHistory(data);
+
+      if (data[0].text.slice(0, 1) === "O") {
+        setXisNext(true);
+      } else if (data[0].text.slice(0, 1) === "X") {
+        setXisNext(false);
+      }
+    });
+  }, [history, xIsNext]);
 
   const handleClick = (i) => {
     let tempSquares = squares.slice(0);
@@ -18,21 +50,36 @@ const Game = () => {
     if (winner || squares[i]) return;
     // Put an X or an O in the clicked square
     tempSquares[i] = xIsNext ? "X" : "O";
+    postReq(tempSquares);
+    logReq(tempSquares[i], i);
     setSquares(tempSquares);
-    setXisNext(!xIsNext);
+    //setXisNext(!xIsNext);
   };
 
   const handleRestart = () => {
     setSquares(Array(9).fill(null));
     setXisNext(true);
+    postReq(Array(9).fill(null));
+    logReq("restart", null);
   };
 
   return (
     <>
       <Board squares={squares} onClick={handleClick} />
-      <div style={styles}>
+      <div style={styles.div1}>
         {winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O")}
-        <button onClick={handleRestart}>Restart</button>
+        <button style={styles.button} onClick={handleRestart}>
+          Restart
+        </button>
+      </div>
+      <div style={styles.div2}>
+        {history
+          ? history.map((log) => (
+              <div key={log._id}>
+                {log.date} <b>{log.text}</b>
+              </div>
+            ))
+          : null}
       </div>
     </>
   );
